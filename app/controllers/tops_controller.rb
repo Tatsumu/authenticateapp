1bcf06f00aec0f08268ffa5bb35d9c066cb46725
tatsumu <t.m.555.tennis@gmail.com>
2015-10-15 20:41:54 +0900

    error

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,5 +2,12 @@ class TopsController < ApplicationController
 	def index
 		@users = User.all
 	end
+	
+	
+	def private
+  		def current_user
+    		@current_user ||= User.find_by_id(session[:user_id])
+  		end
+  	end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,17 +4,17 @@ class UsersController < ApplicationController
 	end
 
 	def create
-		@users =  User.new(user_params)
-		if @users.save
+		@user =  User.new(user_params)
+		if @user.save
 			redirect_to users_path
 		else
 			render 'new'
 		end
 	end
 
-	def private
+	private
 		def user_params
 			params[:user].permit(:email, :password, :password_confirmation)
 		end
-	end
+	
 end
